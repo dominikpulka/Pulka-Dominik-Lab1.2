ba986fd6cd56c114633f839e9237928fd26db612
dominikpulka <48321001+dominikpulka@users.noreply.github.com>
2019-11-25 20:19:32 +0100

    Update Little.js
    Skończyny js :)

--- Projek Paint/Little.js
+++ Projek Paint/Little.js
@@ -1,10 +1,10 @@
 document.addEventListener('DOMContentLoaded', appStart)
 
-let canvas;
-let ctx;
-let movedown = fales;
-let more = "square";
-const color = drawcolor;
+let canvas
+let ctx
+let md = false
+let mode = "kwadrat"
+const color = pickedColor
 
 function appStart(){
     canvas=document.querySelector('#area')
@@ -23,6 +23,44 @@ function appStart(){
     /*document
         .querySelector('#test')
         .addEventListener('click',()=> testFiltr())*/
+    document
+        .querySelector('#square')
+        .addEventListener('click', ()=>{
+            painting()
+            paintingSquare()  
+        })
+    document
+        .querySelector('#circle')
+        .addEventListener('click', ()=>{
+            painting()
+            paintingCircle()
+        })
+    /*document
+        .querySelector('#triangle')
+        .addEventListener('click', ()=>{
+            painting()
+            paintingTriangle()
+        })*/    
+    document
+        .querySelector('#black')
+        .addEventListener('click', ()=> {
+            black()
+        })
+    document
+        .querySelector('#blue')
+        .addEventListener('click', ()=> {
+            blue()
+        })
+    document
+        .querySelector('#red')
+        .addEventListener('click', ()=> {
+            red()
+        })
+    document
+        .querySelector('#green')
+        .addEventListener('click', ()=> {
+            green()
+        })        
     ctx = canvas.getContext('2d')
     drawImage() 
 }
@@ -70,6 +108,7 @@ function negativeFiltr(amount = 30){
     }
     ctx.putImageData(canvasData, 0, 0)
 }
+//Funkcja testowa 
 /*function testFiltr(amount = 30){
     const canvasData = ctx.getImageData(0, 0, 800, 600)
     for(let i = 0; i < canvasData.data.length; i+=4){
@@ -79,3 +118,68 @@ function negativeFiltr(amount = 30){
     }
     ctx.putImageData(canvasData, 0, 0)
 }*/
+function painting(){
+    canvas.addEventListener('mousedown', down)
+    canvas.addEventListener('mouseup', up)
+    canvas.addEventListener('mousemove',
+    function(e){
+        let pos = getMousePos(canvas, e)
+        let x = pos.x
+        let y = pos.y
+        let mode = "circle"
+        draw(canvas, x, y, mode)
+    })
+   
+    function down(){
+        md = true
+    }
+    function up(){
+        md = false
+    }
+    function getMousePos(canvas, e){
+        var rect = canvas.getBoundingClientRect()
+        return{
+            x:e.clientX - rect.left,
+            y:e.clientY - rect.top
+        }
+    }
+    function draw(canvas, x, y){
+        let ctx = canvas.getContext('2d')
+        
+        if(md){
+            if (mode == "circle"){
+                ctx.beginPath()
+                ctx.arc(x, y, 15, 0, 2 * Math.PI)
+                ctx.fill()
+                ctx.fillStyle = pickedColor                
+            }
+            else if (mode== "square"){
+                ctx.fillRect(x, y, 15, 15)
+                ctx.fillStyle = pickedColor
+            }
+        }
+    }
+}
+function paintingCircle()
+{
+    mode = "circle"
+}
+function paintingSquare()
+{
+    mode = "square"
+}
+/*function paintingTriangle(){
+    mode = "triangle"
+}*/
+function black(){
+    pickedColor = 'black'
+}
+function blue(){
+    pickedColor = 'blue'
+}
+function red(){
+    pickedColor = 'red'
+}
+function green(){
+    pickedColor = 'green'
+}
